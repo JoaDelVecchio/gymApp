fae022948e0346b61140f13a110af89290442669
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-12-24 10:16:43 -0300

    Install dependencies

---
+++ src/scenes/navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  const flexBetween = "flex items-center justify-between";
+
+  return (
+    <nav className={`${flexBetween}`}>
+      <p>HI</p>
+      <p>HI</p>
+    </nav>
+  );
+};
+
+export default Navbar;
